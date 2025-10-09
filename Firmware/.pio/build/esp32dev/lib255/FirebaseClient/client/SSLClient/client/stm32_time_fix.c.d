8545bc81fbd84ef30e08bec51ebf547625c49bba
Chí Toàn <81589191+ChitoanRepo@users.noreply.github.com>
2025-10-09 19:15:56 +0700

    Delete Firmware directory

--- Firmware/.pio/build/esp32dev/lib255/FirebaseClient/client/SSLClient/client/stm32_time_fix.c.d
+++
@@ -1,2 +0,0 @@
-.pio/build/esp32dev/lib255/FirebaseClient/client/SSLClient/client/stm32_time_fix.c.o: \
- .pio/libdeps/esp32dev/FirebaseClient/src/client/SSLClient/client/stm32_time_fix.c
--- Firmware/.pio/build/esp32dev/lib8ba/BLE/BLEExceptions.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/esp32dev/lib8ba/BLE/BLEExceptions.cpp.o: \
- C:/Users/nobit/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLEExceptions.cpp
